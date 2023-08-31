35179385b04865d5fb170f1f545303f966782b36
Ajinkyasus <cajinkya659@gmail.com>
2023-08-31 16:58:00 +0530

    commit:7

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class item(models.Model):
+    item_name = models.CharField(max_length=50)
+    item_desc = models.CharField(max_length=50)
+    item_price = models.IntegerField()
+    #create superuser on  monday
+    
