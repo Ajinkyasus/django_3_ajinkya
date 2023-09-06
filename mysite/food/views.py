f10ff13ed3af841d242d6df20d5423c9560bd1b6
Ajinkyasus <cajinkya659@gmail.com>
2023-09-06 15:37:46 +0530

    commit:11

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from food.models import item
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2 style="color:blue">This is a details page</h2>')
+    itemlist = item.objects.all()
+    return HttpResponse(itemlist)
 def detail(request):
     return HttpResponse('<h1 style="color:red">this is the detail page</h1>')
