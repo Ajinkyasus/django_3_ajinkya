1b62aad929f6556c78a0b6b12efadbaae3abe31e
Ajinkyasus <cajinkya659@gmail.com>
2023-09-06 16:32:43 +0530

    commit:16

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -11,5 +11,5 @@ def index(request):
     }
     
     return render(request, 'food/index.html',context)
-def detail(request):
-    return HttpResponse('<h1 style="color:red">this is the detail page</h1>')
+def detail(request, item_id):
+    return HttpResponse('item_id: {}'.format(item_id))
