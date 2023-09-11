b467308cb49852f305acedb9b5ac1a3731d25432
Ajinkyasus <cajinkya659@gmail.com>
2023-09-11 16:10:14 +0530

    commit:19

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from food import views
 
+app_name ="food"
+
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('detail/<int:item_id>/',views.detail,name='detail')
+    path('detail/<int:item_id>/',views.detail,name='detail'),
 ]
